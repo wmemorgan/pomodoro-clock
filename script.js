f883cf63d9f34da596e7c300b5834aac0ad31d61
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-03-14 15:43:52 +0800

    -Add sessionName variable. -Add timer variable. -Add  timeTarget() function -Add countDown() function. -Assign timeTarget function to interval elements.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let decrease = document.getElementsByClassName("interval-decrease");
 let interval = document.getElementsByClassName("interval");
 let increase = document.getElementsByClassName("interval-increase");
+let sessionName = document.getElementsByClassName("session-name");
+let timer = document.getElementsByClassName("timer");
 
 const decreaseTime = (i) => {
   parseInt(interval[i].innerHTML)
@@ -24,10 +26,30 @@ const increaseTime = (i) => {
   }
 };
 
+const timeTarget = (i) => {
+    return new Date().getTime() + parseInt(interval[i].innerHTML) * 60000;
+};
+
+const countDown = () => {
+  let now = new Date().getTime();
+  let intervalLength = timeTarget - now ;
+  let minutes = Math.floor((intervalLength % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((intervalLength % (1000 * 60)) / 1000);
+  console.log("Current time:", now);
+  console.log("Interval length:", intervalLength);
+  console.log("m:", minutes, "s:", seconds);
+  timer[0].innerHTML = minutes + ":" + seconds;
+  // console.log("s:", seconds);
+}
+
 for (let i = 0; i < decrease.length; i++ ) {
   decrease[i].addEventListener("click", decreaseTime(i));
 }
 
 for (let i = 0; i < increase.length; i++) {
   increase[i].addEventListener("click", increaseTime(i));
+}
+
+for (let i = 0; i < interval.length ; i++) {
+  timeTarget(i);
 }
